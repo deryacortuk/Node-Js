4cec594756bdf24cd846eb63beb960769341169a
Derya Cortuk <53267226+deryacortuk@users.noreply.github.com>
2019-11-22 13:03:46 +0300

    Update forecast.js

--- rest.api/utils/forecast.js
+++ rest.api/utils/forecast.js
@@ -1,16 +1,15 @@
 const request = require('request')
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/eeb0a97660650f4b9a5db6a96aefef5c/37.8267,-122.4233'
-    request({ url, json: true }, (err, { body }) => {
-        if (err) {
-            callback('unable to connect to service', undefined)
-        } else if (body.err) {
-            callback('unable to find location', undefined)
+    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.currently.time)
-
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
-
     })
 }
 
